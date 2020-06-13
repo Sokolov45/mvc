f7a702c035c88b6a02a84e5c8b0192e9efbc33d9
Sokolov <zvuk45@bk.ru>
2020-06-13 14:27:45 +0500

    подключил автолоудер

---
+++ core/Application.php
@@ -0,0 +1,16 @@
+<?php
+namespace core;
+
+
+
+
+class Application
+{
+    public function run()
+    {
+
+        include "../app/templates/registerForm.html";
+    }
+
+
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../vendor/autoload.php";
+
+echo 'start';
+$app = new core\Application();
+$app->run();
