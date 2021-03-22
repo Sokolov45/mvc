715c0abcfb74ee70df428ec17360ff826f9a75c0
ÐAnton Sokolov <zvuk45@bk.ru>
2021-03-22 16:52:11 +0500

    6

--- src/Route.php
+++ src/Route.php
@@ -1,6 +1,8 @@
 <?php
 namespace Base;
 
+/*разбирает $_SERVER['REQUEST_URI'] и выданть нам название контроллера и название экшена
+который в нём нужно вызвать.*/
 class Route
 {
     private $controllerName;
@@ -17,38 +19,31 @@ class Route
             $this->controllerName = $this->routes[$path][0];
             $this->actionName = $this->routes[$path][1];
         } else {
-
         };
-
-
         switch ($parts['path']) {
             case '/user/login':
-
                 $controller = new \App\Controller\User();
                 $controller->loginAction();
-
                 break;
 
             case '/user/register':
-
                 $controller = new \App\Controller\User();
                 $controller->registerAction();
-
                 break;
 
             case '/blog':
             case '/blog/index':
-
                 $controller = new \App\Controller\Blog();
                 $controller->indexAction();
-
                 break;
+
             default:
                 header("HTTP/1.0 404  ");
                 break;
         };
     }
 
+//    добавляет в наши роуты имя контроллера и экшена, который нужно выполнить
     public function addRoute($path, $controllerName, $actionName)
     {
         $this->routes[$path] = [
