1780422b345e1fd4aff2118095a2ceb25bb254c2
ÐAnton Sokolov <zvuk45@bk.ru>
2021-03-23 18:06:13 +0500

    6 fin

--- html/index.php
+++ html/index.php
@@ -1,23 +1,29 @@
 <?php
+//это назвается исполняемый файл
 
 use Base\Route;
+use Base\RouteException;
 
 include "../vendor/autoload.php";
 
 $parts = parse_url($_SERVER['REQUEST_URI']);
 
+//Выносим всю логику роутинга внутрь объекта Route, в индексе оставляем только добавление статических(тех, которые указываем напрямую) роутов
 $route = new Route();
-/** @uses \App\Controller\User::loginAction() */
-$route->addRoute('/user/login', \App\Controller\User::class, 'login');
-
+/** @uses \App\Controller\User::loginAction() */ //чтобы можно было искать в find ussages + можно кликнуть по экшену
+$route->addRoute('/user/login', \App\Controller\User::class, 'login'); //User::class - чтобы можно было кликнуть
 /** @uses \App\Controller\User::registerAction() */
-$route->addRoute('/user/login', \App\Controller\User::class, 'register');
-
+$route->addRoute('/user/register', \App\Controller\User::class, 'register');
 /** @uses \App\Controller\Blog::indexAction() */
-$route->addRoute('/ user/login', \App\Controller\User::class, 'index');
+$route->addRoute('/blog', \App\Controller\Blog::class, 'index');
+$route->addRoute('/blog/index', \App\Controller\Blog::class, 'index');
 
 $controllerName  = $route->getControllerName();
 $controller = new $controllerName;
 
 $actionName = $route->getActionName();
+if (!method_exists($controller, $actionName)) {
+    throw new RouteException('Action ' . $actionName . ' not found in ' . $controllerName);
+}
+
 $controller->$actionName();
--- src/Route.php
+++ src/Route.php
@@ -2,45 +2,58 @@
 namespace Base;
 
 /*разбирает $_SERVER['REQUEST_URI'] и выданть нам название контроллера и название экшена
-который в нём нужно вызвать.*/
+который в нём нужно вызвать.
+Делаем его так, чтобы если мы хотим - задаём урлы напрямую, а если они не заданы, то задаётся динамиески
+*/
 class Route
 {
     private $controllerName;
     private $actionName;
-    private $processed = false;
+    private $processed = false; //флаг
     private $routes;
 
     private function process()
     {
         $parts = parse_url($_SERVER['REQUEST_URI']);
         $path = $parts['path'];
-//        if (isset($this->routes[$path])) {
-        if (($route = $this->routes[$path] ?? null) !== null) {
-            $this->controllerName = $this->routes[$path][0];
-            $this->actionName = $this->routes[$path][1];
+
+//        эта часть отвечает за статический роутинг, в противном случае будем пытаться определить динамически
+        if (($route = $this->routes[$path] ?? null) !== null) { //когда нужно запроцессить роутинг мы сначала проверяем есть ли наш path  в массиве routes
+            $this->controllerName = $route[0];    //определяем внутренние переменные
+            $this->actionName = $route[1];    //определяем внутренние переменные
         } else {
+            $parts = explode('/', $path);
+            var_dump($parts);
+            $this->controllerName = '\\App\\Controller' . ucfirst(strtolower($parts[1]));
+            $this->actionName = strtolower($parts[2] ?? 'index');
+
+            if (!class_exists($this->controllerName)) {
+                throw new RouteException('ne mogu naiti controller' . $this->controllerName);
+            }
+
+
         };
-        switch ($parts['path']) {
-            case '/user/login':
-                $controller = new \App\Controller\User();
-                $controller->loginAction();
-                break;
-
-            case '/user/register':
-                $controller = new \App\Controller\User();
-                $controller->registerAction();
-                break;
-
-            case '/blog':
-            case '/blog/index':
-                $controller = new \App\Controller\Blog();
-                $controller->indexAction();
-                break;
-
-            default:
-                header("HTTP/1.0 404  ");
-                break;
-        };
+//        switch ($parts['path']) {
+//            case '/user/login':
+//                $controller = new \App\Controller\User();
+//                $controller->loginAction();
+//                break;
+//
+//            case '/user/register':
+//                $controller = new \App\Controller\User();
+//                $controller->registerAction();
+//                break;
+//
+//            case '/blog':
+//            case '/blog/index':
+//                $controller = new \App\Controller\Blog();
+//                $controller->indexAction();
+//                break;
+//
+//            default:
+//                header("HTTP/1.0 404  ");
+//                break;
+//        };
     }
 
 //    добавляет в наши роуты имя контроллера и экшена, который нужно выполнить
@@ -65,6 +78,6 @@ class Route
         if (!$this->processed) {
             $this->process();
         }
-        return $this->actionName;
+        return $this->actionName . 'Action';
     }
 }
